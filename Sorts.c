e2e90a1f9ae216c794b755b861c4fbe042c3f6ba
Matheus Minski dos Santos <37547306+Minsking@users.noreply.github.com>
2018-11-18 19:23:31 -0200

    Add files via upload

--- Sorts.c
+++ Sorts.c
@@ -193,8 +193,88 @@ void insertion_sort_malloc_cres(int *b, int n){
         *(b+j+1)=aux;
     }
 }
+int busca_linear_normal(int v[], int n, int numpesquisado){
+    int i;
+    for(i=0;i<n;i++){
+        if(numpesquisado==v[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+int busca_linear_malloc(int *b, int n, int numpesquisado){
+    int i;
+    for(i=0;i<n;i++){
+        if(numpesquisado==*(b+i)){
+            return 1;
+        }
+    }
+    return 0;
+}
+int busca_binaria_normal(int vet[], int n, int numpesquisado, char ordem){
+    int i=0,f,m; 
+    f = n-1; 
+    if(ordem=='c'){
+        while(i<=f){ 
+        m = (i+f)/2;
+        if( numpesquisado == vet[m] ){ 
+            return 1;
+        }
+        if(numpesquisado<vet[m]){ // < crescente, > Decrescente
+        f = m-1;
+        }else{
+            i = m+1;
+        }
+    } 
+     return 0; 
+    }else{
+        while(i<=f){ 
+        m = (i+f)/2;
+        if( numpesquisado == vet[m] ){ 
+            return 1;
+        }
+        if(numpesquisado>vet[m]){ // < crescente, > Decrescente
+        f = m-1;
+        }else{
+            i = m+1;
+        }
+    } 
+     return 0; 
+    }
+}
+int busca_binaria_malloc(int *b, int n, int numpesquisado, char ordem){
+    int i=0,f,m; 
+    f = n-1; 
+    if(ordem=='c'){
+        while(i<=f){ 
+        m = (i+f)/2;
+        if( numpesquisado == *(b+m)){ 
+            return 1;
+        }
+        if(numpesquisado<*(b+m)){ // < crescente, > Decrescente
+            f = m-1;
+        }else{
+            i = m+1;
+        }
+    } 
+     return 0; 
+    }else{
+        while(i<=f){ 
+        m = (i+f)/2;
+        if( numpesquisado == *(b+m) ){ 
+            return 1;
+        }
+        if(numpesquisado>*(b+m)){ // < crescente, > Decrescente
+            f = m-1;
+        }else{
+            i = m+1;
+        }
+    } 
+     return 0; 
+    }
+}
 int main(){
-    char letrametodo, ordem, letraalloc, restart;
+    char letrametodo, ordem, letraalloc, restart, numpesquisado, letraBusca, letraBuscaModo, bacana, bacana2;
     int tamvetor, *p;
     printf("Qual metodo deseja usar? ('b', 'i' ou 's')\n");
     scanf("%c", &letrametodo);
@@ -206,8 +286,13 @@ int main(){
     scanf("%c", &letraalloc);
     printf("Qual o tamanho do vetor\n");
     scanf("%d", &tamvetor);
+    int vet[tamvetor];
+    p=malloc(tamvetor*sizeof(int));
+        if(!p){
+            printf("Tamanho de memória insuficiente");
+            exit(1);
+        }
     if(letraalloc=='n'){
-        int vet[tamvetor];
         printf("Digite seu vetor de tamanho %d\n", tamvetor);
         ler_vetor_normal(vet, tamvetor);
         switch(letrametodo){
@@ -223,11 +308,6 @@ int main(){
             }
         mostrar_vetor_normal(vet, tamvetor);
     }else{
-        p=malloc(tamvetor*sizeof(int));
-        if(!p){
-            printf("Tamanho de memória insuficiente");
-            exit(1);
-        }
         printf("Digite seu vetor de tamanho %d\n", tamvetor);
         ler_vetor_malloc(p, tamvetor);
         switch(letrametodo){
@@ -242,11 +322,34 @@ int main(){
                 break;
         }
         mostrar_vetor_malloc(p, tamvetor);
-        free(p);
+    }
+    printf("\nDeseja fazer uma busca? ('s' ou 'n')\n");
+    fflush(stdin);
+    scanf("%c", &letraBusca);
+    if(letraBusca=='s'){
+        bacana=letraalloc;
+        bacana2=ordem;
+        printf("Binaria ou linear? ('b' ou 'l')\n");
+        fflush(stdin);
+        scanf("%c", &letraBuscaModo);
+        printf("Qual o numero procurado?\n");
+        fflush(stdin);
+        scanf("%d", &numpesquisado);
+        if((letraBuscaModo=='l') && (bacana=='n')){
+            (busca_linear_normal(vet, tamvetor, numpesquisado)) ? printf("Seu numero existe") : printf("Seu numero nao existe");
+        }if((letraBuscaModo=='l') && (bacana=='s')){
+            (busca_linear_malloc(p, tamvetor, numpesquisado)) ? printf("Seu numero existe") : printf("Seu numero nao existe");
+        }if((letraBuscaModo=='b') && (bacana=='n')){
+            (busca_binaria_normal(vet, tamvetor, numpesquisado, bacana2)) ? printf("Seu numero existe") : printf("Seu numero nao existe");
+        }if((letraBuscaModo=='b') && (bacana=='s')){
+            (busca_binaria_malloc(p, tamvetor, numpesquisado, bacana2)) ? printf("Seu numero existe") : printf("Seu numero nao existe");
+        }
     }
     printf("\nDeseja continuar operando? ('s' ou 'n')\n");
     fflush(stdin);
     scanf("%c", &restart);
     fflush(stdin);
+    free(p);
+    p=NULL;
     restart=='s' ? main() : exit(0);
 }
